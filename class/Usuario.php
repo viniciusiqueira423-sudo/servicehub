7f11de02bba5888e7c01f840d2a7fe46f740543d
Vinicius.SC <viniciusiqueira423@gmail.com>
2026-04-22 13:42:07 -0300

    atualização de senha.php e usuario.php seguindo como base a aula anterior

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,9 +1,8 @@
-<?php
+<?php 
 // incluir a conexão
 include_once "config/conexao.php";
 // declara a classe
-class Usuario
-{
+class Usuario{
     // atributos
     private $id;
     private $nome;
@@ -14,93 +13,138 @@ class Usuario
     private $primeiro_login;
     private $pdo;
     // construtor
-    public function __construct()
-    {
-        $this->pdo = obterPdo();
+    public function __construct(){
+       $this->pdo = obterPdo();
     }
     // Getters / Setters
-    public function getId()
-    {
+    public function getId(){
         return $this->id;
     }
-    public function getNome()
-    {
+    public function getNome(){
         return $this->nome;
     }
-    public function setNome(string $nome)
-    {
+    public function setNome(string $nome){
         $this->nome = $nome;
     }
-    public function getEmail()
-    {
+    public function getEmail(){
         return $this->email;
     }
-    public function setEmail(string $email)
-    {
+    public function setEmail(string $email){
         $this->email = $email;
     }
-    public function getSenha()
-    {
+    public function getSenha(){
         return $this->senha;
     }
-    public function setSenha(string $senha)
-    {
+    public function setSenha(string $senha){
         $this->senha = $senha;
     }
-    public function getTipo()
-    {
+    public function getTipo(){
         return $this->tipo;
     }
-    public function setTipo(string $tipo)
-    {
+    public function setTipo(string $tipo){
         $this->tipo = $tipo;
     }
-    public function getAtivo()
-    {
+    public function getAtivo(){
         return $this->ativo;
     }
-    public function setAtivo(string $ativo)
-    {
+    public function setAtivo(string $ativo){
         $this->ativo = $ativo;
     }
-    public function getPrimeiroLogin()
-    {
+    public function getPrimeiroLogin(){
         return $this->primeiro_login;
     }
-    public function setPrimeiroLogin(string $primeiro_login)
-    {
+    public function setPrimeiroLogin(bool $primeiro_login){
         $this->ativo = $primeiro_login;
     }
-    // métodos (functions)  - Representam os RFs do projeto
+    // métodos (functions) - Representam os RFs do projeto
     // efetuar login
-    public static function efetuarLogin(string $email, string $senha): array
-    {
+    public static function efetuarLogin(string $email, string $senha):array{
         $sql = "select * from usuarios where email = :email and ativo = b'1'";
         $cmd = obterPdo()->prepare($sql);
         $cmd->bindValue(":email", $email);
         $cmd->execute();
         $dados = $cmd->fetch(PDO::FETCH_ASSOC);
-        if ($dados && password_verify($senha, $dados['senha'])) {
+        if($dados && password_verify($senha, $dados['senha'])){
             return $dados;
-        } else {
-            return $dados = [];
+        }else{
+            return $dados = [];    
         }
     }
-
-    //inserir
-    public function inserir(): bool
-    {
-        $sql = "INSERT usuarios (nome, email, senha, tipo) values (:nome, :email, :senha, :tipo)";
+    // inserir 
+    // o método inserir deve retornar um booleano (true ou false) para indicar se a operação foi bem sucedida ou não
+    public function inserir():bool{
+        $sql = "INSERT usuarios (nome, email, senha, tipo)
+         values (:nome, :email, :senha, :tipo)";
         $cmd = $this->pdo->prepare($sql);
         $cmd->bindValue(":nome", $this->nome);
         $cmd->bindValue(":email", $this->email);
-        $cmd->bindValue(":senha", password_hash($this->senha, PASSWORD_DEFAULT));
+        $cmd->bindValue(":senha", password_hash($this->senha,PASSWORD_DEFAULT));
         $cmd->bindValue(":tipo", $this->tipo);
-        if ($cmd->execute()) {
+        if($cmd->execute()){
             $this->id = $this->pdo->lastInsertId();
             return true;
         }
         return false;
-
+    }    
+    // Listar
+    // o método listar deve retornar um array de usuários (array associativo)
+    public static function listar():array{
+        $cmd = obterPdo()->query("select * from usuarios order by id desc");
+        return $cmd->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    // buscar por id
+    public function buscarPorId(int $id):bool{
+        $sql = "SELECT * FROM usuarios WHERE id = :id";
+        $cmd = obterPdo()->prepare($sql);
+        $cmd->bindValue(":id",$id);
+        $cmd->execute();
+        if($cmd->rowCount() > 0){
+            $dados = $cmd->fetch(PDO::FETCH_ASSOC);
+            // var_dump($dados);
+            // die();
+            $this->id = $dados['id'];
+            $this->setNome($dados['nome']);
+            $this->setEmail($dados['email']);
+            $this->setSenha($dados['senha']);
+            $this->setTipo($dados['tipo']);
+            $this->setAtivo($dados['ativo']);
+            $this->primeiro_login = $dados['primeiro_login'];
+            return true;
+        }
+        return false;
+    }
+    // Atualizar
+    public function atualizar():bool{
+        if(!$this->id) return false;
+        // var_dump($this->id);
+        // die();
+        $sql = "UPDATE usuarios
+                set nome = :nome, email = :email, tipo = :tipo, ativo = :ativo,
+                primeiro_login = :primeiro_login
+                WHERE id = :id";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(":id", $this->id); 
+        $cmd->bindValue(":nome", $this->nome);   
+        $cmd->bindValue(":email", $this->email);   
+        $cmd->bindValue(":tipo", $this->tipo);   
+        $cmd->bindValue(":ativo", $this->ativo, PDO::PARAM_BOOL);   
+        $cmd->bindValue(":primeiro_login", $this->primeiro_login, PDO::PARAM_BOOL);   
+        return $cmd->execute();
+    }
+
+    // Atualizar senha (já deve vir com password_hash)
+    // o método atualizarSenha deve retornar um booleano (true ou false) para indicar se a operação foi bem sucedida ou não
+    public function atualizarSenha(string $senhaHash):bool{
+        if(!$this->id) return false;
+
+        $sql = "UPDATE usuarios SET senha = :senha WHERE id = :id";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(":senha", $senhaHash);
+        $cmd->bindValue(":id", $this->id, PDO::PARAM_INT);
+
+     
+        return $cmd->execute();
+    }
+    
 }
--- senha.php
+++ senha.php
@@ -1,16 +1,19 @@
 <?php 
 // $senha = password_hash("123456",PASSWORD_DEFAULT);
 // echo $senha;
-
+//mudança de senha: o usuário deve informar a senha atual para poder alterar a senha, 
+//e a nova senha deve ser confirmada (digitar a nova senha duas vezes para evitar erros de digitação)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once "class/Usuario.php";
 
-$usuario = new Usuario();
-$usuario->setNome("Vinicus Siqueira");
-$usuario->setEmail("vinicusiqueira423@gmail.com");
-$usuario->setSenha("Vini_423");
-$usuario->setTipo(2);
-
-if ($usuario->inserir()) {
-    echo "Usuario". $usuario->getNome()." inserido com sucesso com o ID: ". $usuario->getId();
+// testando update
+$usuario = new Usuario();// objeto vazio
+$usuario->buscarPorId(56);
+if($usuario->atualizarSenha(password_hash("123456", PASSWORD_DEFAULT))){
+    echo "Senha do usuário ".$usuario->getNome()." atualizada com sucesso!";
 }
+
+?>
